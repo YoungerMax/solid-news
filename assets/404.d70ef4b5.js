049bdc6363b6988fb7dd673c3757745f71fd5dd6
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2022-08-12 01:40:30 +0000

    Deploying to gh-pages from @ YoungerMax/solid-news@852fcf804ee0d8859ba3d74b93048088d046352d 🚀

---
+++ assets/404.d70ef4b5.js
@@ -0,0 +1,1 @@
+import{E as o}from"./error.a6b09dce.js";import{c as r}from"./index.1d31a622.js";function n(){return r(o,{children:"This page does not exist."})}export{n as default};
---
+++ assets/error.a6b09dce.js
@@ -0,0 +1,1 @@
+import{r as o,i as e,c as l,E as c,t as s}from"./index.1d31a622.js";const d=s('<div class="p-4 border-red-500 text-red-500 fill-red-500 rounded border-1 flex items-center gap-2"></div>');function i(t){const n=o(()=>t.children);return(()=>{const r=d.cloneNode(!0);return e(r,l(c,{}),null),e(r,n,null),r})()}export{i as E};
