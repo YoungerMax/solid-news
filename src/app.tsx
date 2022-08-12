49523747e784dce9bd4568ff6dae62d703be4060
youngermax <60242131+YoungerMax@users.noreply.github.com>
2022-08-11 22:29:25 -0400

    SEO

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,11 @@ import { Component, onCleanup, onMount } from 'solid-js';
 import { Link, useRoutes } from 'solid-app-router';
 
 import { routes } from './routes';
-import favicon from "./img/favicon.png";
 import { SettingsIcon } from './ionicons';
-
 import AppState from './state';
+
+import favicon from "./img/favicon.png";
+
 
 const App: Component = () => {
 	const Route = useRoutes(routes);
@@ -21,7 +22,7 @@ const App: Component = () => {
 			<main class="container sm:mx-auto sm:w-2/3 p-4 dark:bg-dark-800">
 				<div class="mb-4 sm:my-10 flex items-center">
 					<Link href="/" class="flex gap-2 sm:gap-6 items-center" style={{ 'text-decoration': 'none' }}>
-						<img src={favicon} class="w-8 sm:w-16 rounded" />
+						<img src='/favicon.png' class="w-8 sm:w-16 rounded" />
 						<h1 class="text-xl sm:text-5xl font-bold sm:font-font-extrabold">Solid News</h1>
 					</Link>
 
